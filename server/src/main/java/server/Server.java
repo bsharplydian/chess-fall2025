399d679cdad53593c375532edb14c994eeb06dc3
Jacob <jmemmott13@gmail.com>
2025-09-22 13:57:19 -0600

    add: server skeleton

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,46 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.handlers.*;
 
 public class Server {
-
+    UserJsonHandler userHandler = new UserJsonHandler();
+    GameJsonHandler gameHandler = new GameJsonHandler();
     private final Javalin javalin;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        javalin.post("/user", this::register);
+        javalin.post("/session", this::login);
+        javalin.delete("/session", this::logout);
+        javalin.get("/game", this::listGames);
+        javalin.post("/game", this::createGame);
+        javalin.put("/game", this::joinGame);
+        javalin.delete("/db", this::clear);
+    }
+
+    private void register(Context context) {
+
+    }
+    private void login(Context context) {
+
+    }
+    private void logout(Context context) {
+
+    }
+    private void listGames(Context context) {
+
+    }
+    private void createGame(Context context) {
+
+    }
+    private void joinGame(Context context) {
+
+    }
+    private void clear(Context context) {
 
     }
 
