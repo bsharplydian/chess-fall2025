9faca3bd33544837a0851255c89b4c0f6f359823
Jacob <jmemmott13@gmail.com>
2025-10-01 13:42:43 -0600

    add: several gameService tests

--- server/src/test/java/service/GameTests.java
+++ server/src/test/java/service/GameTests.java
@@ -4,12 +4,12 @@ import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
 import dataaccess.exceptions.UnauthorizedException;
-import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import service.requests.CreateGameRequest;
+import service.requests.JoinGameRequest;
 import service.requests.ListGamesRequest;
 import service.requests.RegisterRequest;
 import service.results.CreateGameResult;
@@ -20,21 +20,26 @@ public class GameTests {
     static MemoryAuthDAO authDAO = new MemoryAuthDAO();
     private static UserService userService =  new UserService(authDAO, new MemoryUserDAO());
     private static GameService gameService = new GameService(authDAO, new MemoryGameDAO());
-    private static String authToken;
+    private static String authToken1;
+    private static String authToken2;
 
     @BeforeEach
     void reset() {
         userService.clear();
         gameService.clear();
-        RegisterRequest existingUser = new RegisterRequest("existingUser", "pw", "iexist@mailmail.com");
-        RegisterResult registerResult = userService.register(existingUser);
-        authToken = registerResult.authToken();
+        RegisterRequest user1 = new RegisterRequest("user1", "pw", "user1@mailmail.com");
+        RegisterResult registerResult1 = userService.register(user1);
+        authToken1 = registerResult1.authToken();
+
+        RegisterRequest user2 = new RegisterRequest("user2", "pw", "user2@mailmail.com");
+        RegisterResult registerResult2 = userService.register(user2);
+        authToken2 = registerResult2.authToken();
     }
 
     @Test
     @DisplayName("create game success")
     public void CreateGameSuccess() {
-        CreateGameRequest request = new CreateGameRequest(authToken, "newGame");
+        CreateGameRequest request = new CreateGameRequest(authToken1, "newGame");
         CreateGameResult result = Assertions.assertDoesNotThrow(() -> gameService.createGame(request));
         Assertions.assertNotNull(gameService.gameDAO.getGame(result.gameID()));
     }
@@ -52,14 +57,42 @@ public class GameTests {
     @Test
     @DisplayName("list games success")
     public void ListGamesSuccess() {
-        CreateGameRequest request1 = new CreateGameRequest(authToken, "game1");
+        CreateGameRequest request1 = new CreateGameRequest(authToken1, "game1");
         gameService.createGame(request1);
-        CreateGameRequest request2 = new CreateGameRequest(authToken, "game2");
+        CreateGameRequest request2 = new CreateGameRequest(authToken1, "game2");
         gameService.createGame(request2);
 
-        ListGamesRequest listRequest = new ListGamesRequest(authToken);
+        ListGamesRequest listRequest = new ListGamesRequest(authToken1);
         ListGamesResult listResult = Assertions.assertDoesNotThrow(() -> gameService.listGames(listRequest));
 
         Assertions.assertEquals(2, listResult.games().size(), "listGames returned incorrect number of games");
     }
+
+    @Test
+    @DisplayName("list games unauthorized")
+    public void ListGamesNoAuth() {
+        CreateGameRequest request1 = new CreateGameRequest(authToken1, "game1");
+        gameService.createGame(request1);
+        CreateGameRequest request2 = new CreateGameRequest(authToken1, "game2");
+        gameService.createGame(request2);
+
+        ListGamesRequest listRequest = new ListGamesRequest("");
+        Assertions.assertThrows(UnauthorizedException.class, () -> gameService.listGames(listRequest));
+    }
+
+    @Test
+    @DisplayName("join game success (white)")
+    public void JoinGameSuccess() {
+        CreateGameRequest createRequest = new CreateGameRequest(authToken1, "newGame");
+        int gameID = Assertions.assertDoesNotThrow(() -> gameService.createGame(createRequest)).gameID();
+
+        JoinGameRequest request = new JoinGameRequest(authToken1, chess.ChessGame.TeamColor.WHITE, gameID);
+        Assertions.assertDoesNotThrow(() -> gameService.joinGame(request));
+        Assertions.assertNotNull(gameService.gameDAO.getGame(gameID).whiteUser());
+    }
+    //join game taken (white)
+    //join game success (black)
+    //join game taken (black)
+    //join game unauthorized
+
 }
