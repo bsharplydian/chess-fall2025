e26e9302a7d0eb031ebc9c796a3702fb5dd11d27
Jacob <jmemmott13@gmail.com>
2025-11-11 13:42:26 -0700

    add: register happy path all the way through

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,4 +1,31 @@
 package ui;
 
-public class PreLoginClient {
+import model.requests.RegisterRequest;
+import model.results.RegisterResult;
+import serverfacade.HttpResponseException;
+import serverfacade.ServerFacade;
+
+public class PreLoginClient implements Client {
+    ServerFacade facade;
+    public PreLoginClient(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    public String eval(String input) {
+        String[] params = input.split(" ");
+        try {
+            return switch (params[0]) {
+                case "register" -> handleRegister(params);
+
+                default -> throw new HttpResponseException("Invalid Command: " + params[0]);
+            };
+        } catch (HttpResponseException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+    private String handleRegister(String[] params) throws HttpResponseException {
+        RegisterRequest request = new RegisterRequest(params[1], params[2], params[3]);
+        RegisterResult result = facade.register(request);
+        return "Registered " + result.username();
+    }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,21 +1,27 @@
 package ui;
 
+import serverfacade.ServerFacade;
+
 import java.util.Scanner;
 import static ui.EscapeSequences.*;
 
 public class Repl {
+    ServerFacade facade = new ServerFacade(8080);
+    PreLoginClient client = new PreLoginClient(facade);
+
     public void run() {
-        System.out.println("Welcome to C h e s s; sign in to get started");
+        System.out.println("Welcome to Chess; sign in to get started");
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while(!result.equals("quit")) {
             printPrompt();
-            result = "quit";
+            result = client.eval(scanner.nextLine());
+            System.out.print(SET_TEXT_COLOR_WHITE + result);
         }
 
     }
     private void printPrompt() {
-        System.out.print(RESET_TEXT_COLOR + ">> " + SET_TEXT_COLOR_GREEN);
+        System.out.print("\n" + RESET_TEXT_COLOR + ">> " + SET_TEXT_COLOR_GREEN);
     }
 }
